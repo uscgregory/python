3a2ee789665b1218c9d9dba360864835fabaa9e4
uscgregory <86125891+uscgregory@users.noreply.github.com>
2024-01-04 08:32:17 -0600

    Update code_sample_1.py

--- code_sample_1.py
+++ code_sample_1.py
@@ -10,43 +10,28 @@ from google.oauth2 import service_account
 from zipfile import ZipFile, ZIP_DEFLATED
 import io
 
-DATA_AIRPLANE_SERVICE_ACCOUNT = os.getenv('DATA_AIRPLANE_SERVICE_ACCOUNT')
-SFTP_GDAY_USER = os.getenv('SFTP_GDAY_USER')
-SFTP_GDAY_PASSWORD = os.getenv('SFTP_GDAY_PASSWORD')
-KEY_PATH = os.getenv('KEY_PATH')
-SFTP_VERISK_USER = os.getenv('SFTP_VERISK_USER')
-SFTP_VERISK_PASSWORD = os.getenv('SFTP_VERISK_PASSWORD')
+SERVICE_ACCOUNT = os.getenv('SERVICE_ACCOUNT')
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
 
 @airplane.task(
-    slug="verisk_sftp_transmit",
-    name="Verisk SFTP Transmit",
+    slug="sftp_transmit",
+    name="SFTP Transmit",
     resources=[
         airplane.Resource("data_airplane"),
      ],
     env_vars=[
         airplane.EnvVar(
-            name="DATA_AIRPLANE_SERVICE_ACCOUNT",
-            config_var_name="DATA_AIRPLANE_SERVICE_ACCOUNT",
+            name="SERVICE_ACCOUNT",
+            config_var_name="SERVICE_ACCOUNT",
         ),                
         airplane.EnvVar(
-            name="SFTP_GDAY_USER",
-            config_var_name="SFTP_GDAY_USER",
+            name="USER",
+            config_var_name="USER",
         ),
         airplane.EnvVar(
-            name="SFTP_GDAY_PASSWORD",
-            config_var_name="SFTP_GDAY_PASSWORD",
-        ),
-        airplane.EnvVar(
-            name="KEY_PATH",
-            config_var_name="KEY_PATH",
-        ),
-        airplane.EnvVar(
-            name="SFTP_VERISK_USER",
-            config_var_name="SFTP_VERISK_USER",
-        ),
-        airplane.EnvVar(
-            name="SFTP_VERISK_PASSWORD",
-            config_var_name="SFTP_VERISK_PASSWORD",
+            name="PASSWORD",
+            config_var_name="PASSWORD",
         ),
     ],
 )
@@ -56,10 +41,10 @@ def verisk_sftp_transmit(file_names: str):
     month_year_created = str(datetime.now().strftime("%Y-%m"))
     
     # Setup Google Cloud Storage Credentials 
-    credentials_dict = json.loads(DATA_AIRPLANE_SERVICE_ACCOUNT)
+    credentials_dict = json.loads(SERVICE_ACCOUNT)
     credentials = service_account.Credentials.from_service_account_info(credentials_dict)
     client = storage.Client(credentials=credentials)
-    bucket = client.bucket('branch-data-office')
+    bucket = client.bucket('bucket')
 
     # run queries and upload to GCS bucket
     file_name_list = json.loads(file_names)
@@ -69,7 +54,7 @@ def verisk_sftp_transmit(file_names: str):
             query=
                 """
                 EXPORT DATA OPTIONS(
-                    uri='gs://branch-data-office/Verisk/{}_{}_*.csv',
+                    uri='gs://xxx/sftp/{}_{}_*.csv',
                     format='CSV',
                     overwrite=true,
                     header=true,
@@ -78,43 +63,43 @@ def verisk_sftp_transmit(file_names: str):
                 SELECT * FROM {}
                 """.format(filename['file_name'], date_time_created, filename['file_name'])
         )
-        blobs_to_merge = list(bucket.list_blobs(prefix='Verisk/{}_{}'.format(filename['file_name'], date_time_created)))
-        destination_blob = bucket.blob('Verisk/verisk_reporting_{}_cache_{}.csv'.format(filename['file'], date_time_created))      
+        blobs_to_merge = list(bucket.list_blobs(prefix='sftp/{}_{}'.format(filename['file_name'], date_time_created)))
+        destination_blob = bucket.blob('sftp/sftp_reporting_{}_cache_{}.csv'.format(filename['file'], date_time_created))      
         with destination_blob.open("w") as dest_file:
             for blob in blobs_to_merge:
                 blob_content = blob.download_as_text()
                 dest_file.write(blob_content + '\n')  
                 blob.delete()
         airplane.execute("{}_append".format(filename['file_name']))
-    airplane.execute("verisk_reporting_master_bordereaux_airplane_append")
+    airplane.execute("airplane_reconcile")
     
     # Zip files in GCS Verisk folder
-    blobs = list(bucket.list_blobs(prefix='Verisk/verisk'))
+    blobs = list(bucket.list_blobs(prefix='sftp/sftp'))
     with io.BytesIO() as zip_buffer:
         with ZipFile(zip_buffer, 'w', compression=ZIP_DEFLATED) as zip:
             for blob in blobs:
                 data = blob.download_as_string()
                 zip.writestr(blob.name.split('/', 1)[1], data)
                 blob.delete()
-        bucket.blob('Verisk/verisk_{}.zip'.format(date_time_created)).upload_from_string(zip_buffer.getvalue())
+        bucket.blob('sftp/sftp_{}.zip'.format(date_time_created)).upload_from_string(zip_buffer.getvalue())
 
     # Connect to Files.com (formerly ExaVault) SFTP and transfer zip file
     try:
-        exa_vault_ssh_client = paramiko.SSHClient()
-        exa_vault_ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        exa_vault_ssh_client.connect(hostname="ourbranch.exavault.com", port=22, username=SFTP_GDAY_USER, password=SFTP_GDAY_PASSWORD)
-        exa_vault_ftp_client = exa_vault_ssh_client.open_sftp()
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname="xxx", port=22, username=USER, password=PASSWORD)
+        ftp_client = ssh_client.open_sftp()
 
         try:
-            exa_vault_ftp_client.chdir('/verisk-PDP/'+month_year_created+'/')
+            ftp_client.chdir('/xxx/'+month_year_created+'/')
         except:
-            exa_vault_ftp_client.mkdir('/verisk-PDP/'+month_year_created+'/')
-            exa_vault_ftp_client.chdir('/verisk-PDP/'+month_year_created+'/')  
+            ftp_client.mkdir('/xxx/'+month_year_created+'/')
+            ftp_client.chdir('/xxx/'+month_year_created+'/')  
     
-        zip_blob = bucket.blob('Verisk/verisk_{}.zip'.format(date_time_created))
+        zip_blob = bucket.blob('sftp/sftp_{}.zip'.format(date_time_created))
         file_contents = zip_blob.download_as_string()
-        exa_vault_file_name = 'verisk_{}.zip'.format(date_time_created)
-        with exa_vault_ftp_client.open(exa_vault_file_name, 'wb') as f:
+        file_name = 'verisk_{}.zip'.format(date_time_created)
+        with ftp_client.open(file_name, 'wb') as f:
             f.write(file_contents)
 
     except paramiko.SSHException as ssh_e:
@@ -124,9 +109,9 @@ def verisk_sftp_transmit(file_names: str):
         print(f"OS error occurred: {str(os_e)}")
 
     finally:
-        exa_vault_ftp_client.close()
-        exa_vault_ssh_client.close()
+        ftp_client.close()
+        ssh_client.close()
 
-    delete_blobs = bucket.list_blobs(prefix='Verisk/verisk')
+    delete_blobs = bucket.list_blobs(prefix='sftp/sftp')
     for blob in delete_blobs:
         blob.delete()
